f216990fbe5fbb98d19e59b798bf7a6840a9266a
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-18 11:27:17 +0530

    Added singleton class and called them in main class

--- Singletonpattern/ChuckyKills.cs
+++ Singletonpattern/ChuckyKills.cs
@@ -1,45 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Singletonpattern
+namespace SingletonPattern
 {
     public class ChuckyKills
     {
-        private ChuckyKills _instance = null;
+        private static ChuckyKills _instance = null;
         private int _killcount = 0;
 
-        private ChuckyKills() 
-        { 
+        private ChuckyKills()
+        {
         }
-        
+
         public static ChuckyKills Instance
         {
             get
             {
-                if (_instance == null)
-                {
-                    _instance = new ChuckyKills();
-                }
+
+                    if (_instance == null)
+                    {
+                        _instance = new ChuckyKills();
+                    }
+                    return _instance;
             }
-            return _instance;
         }
-    }
 
-    public void KillsCount()
-    {
-        _killcount += 1;
-        Console.WriteLine("Total kills #" + _killcount);
-
-    }
-
-    public int TotalKills
-    {
-        get
+        public void AddKill()
         {
-            return _killcount;
+            _killcount++;
+            Console.WriteLine("Total kills #" + _killcount);
+        }
+
+        public int TotalKills
+        {
+            get { return _killcount; }
         }
     }
 }
---
+++ Singletonpattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using SingletonPattern;
+using System;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChuckyKills chucky = ChuckyKills.Instance;
+            chucky.AddKill();
+            chucky.AddKill();
+            chucky.AddKill();
+            chucky.AddKill();
+            chucky.AddKill();
+
+            Console.WriteLine("Total kills: " + chucky.TotalKills);
+
+            
+            ChuckyKills anotherChucky = ChuckyKills.Instance;
+            anotherChucky.AddKill();
+
+            Console.WriteLine("Total kills: " + chucky.TotalKills);
+            Console.WriteLine("Total kills from another instance: " + anotherChucky.TotalKills);
+        }
+    }
+}
