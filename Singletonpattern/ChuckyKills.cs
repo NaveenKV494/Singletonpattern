45f527b8d5eea3d47b0afda21402d4647eba9b0f
TLSPC-079 <TLSPC-079@DESKTOP-AVJ9472>
2023-11-18 11:07:21 +0530

    Created a class and added elements to it

---
+++ Singletonpattern/ChuckyKills.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletonpattern
+{
+    public class ChuckyKills
+    {
+        private ChuckyKills _instance = null;
+        private int _killcount = 0;
+
+        private ChuckyKills() 
+        { 
+        }
+        
+        public static ChuckyKills Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ChuckyKills();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public void KillsCount()
+    {
+        _killcount += 1;
+        Console.WriteLine("Total kills #" + _killcount);
+
+    }
+
+    public int TotalKills
+    {
+        get
+        {
+            return _killcount;
+        }
+    }
+}
